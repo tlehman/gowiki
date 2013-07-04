f52b3a21c2cad0e65d62d14335d996dab0f3cd76
Tobi Lehman <tobi.lehman@gmail.com>
2013-07-04 15:57:28 -0700

    redirect root to view/FrontPage

--- wiki.go
+++ wiki.go
@@ -59,6 +59,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
+
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		title := r.URL.Path[lenPath:]
@@ -79,6 +83,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 
 func main() {
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
