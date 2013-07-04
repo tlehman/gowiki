ef68fa23ad4c9c89dda989edca937f36df6cca96
Tobi Lehman <tobi.lehman@gmail.com>
2013-07-04 02:30:27 -0700

    add loadPage

--- wiki.go
+++ wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"io/ioutil"
 )
 
@@ -16,8 +16,22 @@ func (p *Page) save() error {
     return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+// to fetch the page from disk
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
 
 func main() {
-    page := &Page{"cats", []byte("an amusing organism\n")}
-    page.save()    
+    page, err := loadPage("pandas")
+    if err != nil {
+        fmt.Printf("%v\n", err)
+    } else {
+        fmt.Printf("<h1>%s</h1><br><p>%s</p>\n", page.Title, page.Body)    
+    }
 }
