ed5ee93b2fb93cb94ede660d4968218a16ece94e
Tobi Lehman <tobi.lehman@gmail.com>
2013-07-04 14:20:42 -0700

    template caching

--- wiki.go
+++ wiki.go
@@ -7,6 +7,7 @@ import (
 )
 
 const lenPath = len("/view/")
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 type Page struct {
 	Title string
@@ -48,12 +49,7 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
